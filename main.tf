a4d030ee6d81220cfbe0f0457dbce0295436d006
Dmitriy Marinyuk <mdv-devops@gmail.com>
2023-03-10 13:59:06 +0400

    Fix

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
-  //  backend "s3" {
-  //    bucket         = "boints-terraform-state"
-  //    key            = "kubernetes/winmoney-stage/terraform.tfstate"
-  //    region         = "us-east-1"
-  //    dynamodb_table = "tfstate_locks"
-  //    encrypt        = true
-  //  }
+  backend "s3" {
+    bucket         = "boints-terraform-state"
+    key            = "kubernetes/winmoney-stage/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tfstate_locks"
+    encrypt        = true
+  }
   required_providers {
     godaddy = {
       source  = "n3integration/godaddy"
